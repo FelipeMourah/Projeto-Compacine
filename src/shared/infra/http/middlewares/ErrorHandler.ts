46f3ad20c9100279fcb5809048b5682d9f34bb6d
EmanuelErnesto <emanuel.earego@gmail.com>
2024-06-05 02:11:36 -0300

    feat: add specific Errors Handler

---
+++ src/shared/infra/http/middlewares/ErrorHandler.ts
@@ -0,0 +1,15 @@
+import AppError from '@shared/errors/AppError';
+import { NextFunction, Request, Response } from 'express';
+
+export default function ErrorHandler(
+  error: AppError,
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Response {
+  return response.status(error.code).json({
+    code: error.code,
+    status: error.status,
+    message: error.message,
+  });
+}
