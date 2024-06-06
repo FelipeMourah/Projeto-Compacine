d44b81be013d7dc79c0cf937753341dc2c4a6d74
Maicon Luís Bresolin <maiconbresolin66@gmail.com>
2024-06-06 12:31:24 -0300

    feat: add sessions entity

---
+++ src/modules/sessions/infra/entities/Sessions.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Movie from '@modules/movies/infra/typeorm/entities/Movies';
+
+@Entity('sessions')
+export class Session {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('uuid')
+  movie_id: string;
+
+  @ManyToOne(() => Movie)
+  @JoinColumn({ name: 'movie_id' })
+  movie: Movie;
+
+  @Column()
+  room: string;
+
+  @Column()
+  capacity: number;
+
+  @Column({ type: 'date' })
+  day: Date;
+
+  @Column()
+  time: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
