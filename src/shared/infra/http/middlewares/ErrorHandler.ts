3295e99ddbe09de8d9c420a0d70a9e50438f8ae3
EmanuelErnesto <emanuel.earego@gmail.com>
2024-06-07 02:15:46 -0300

    refator: receive internal server error handler

--- src/shared/infra/http/middlewares/ErrorHandler.ts
+++ src/shared/infra/http/middlewares/ErrorHandler.ts
@@ -2,15 +2,24 @@ import AppError from '@shared/errors/AppError';
 import { NextFunction, Request, Response } from 'express';
 
 export default function ErrorHandler(
-  error: AppError,
+  error: Error,
   request: Request,
   response: Response,
   next: NextFunction,
 ): Response {
-  return response.status(error.code).json({
-    code: error.code,
-    status: error.status,
-    message: error.message,
-    details: error.details,
+  if (error instanceof AppError) {
+    return response.status(error.code).json({
+      code: error.code,
+      status: error.status,
+      message: error.message,
+      details: error.details,
+    });
+  }
+  return response.status(500).json({
+    code: 500,
+    status: 'Internal Server Error',
+    message:
+      'The server has encountered a situation it does not know how to handle',
+    details: error.message,
   });
 }
