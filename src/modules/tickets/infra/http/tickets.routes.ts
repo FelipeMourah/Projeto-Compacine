e71362576d535d51391298e8ddceef05b845a299
Valmir Thume Junior <juninhothume21@gmail.com>
2024-06-06 21:11:27 -0300

    build: Add routes to Ticket resource

---
+++ src/modules/tickets/infra/http/tickets.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+const ticketsRouter = Router();
+
+ticketsRouter.post('/', (req, res) => {
+  // Replace with TicketController
+})
+
+ticketsRouter.put('/:id', (req, res) => {
+  // Replace with TicketController
+})
+
+ticketsRouter.delete('/:id', (req, res) => {
+  // Replace with TicketController
+})
+
+
+export default ticketsRouter;
