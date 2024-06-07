b4b6c4c1b9d550c947bed31b017e819f31d979c6
Valmir Thume Junior <juninhothume21@gmail.com>
2024-06-07 09:19:04 -0300

    fix: Corrects the definition of interfaces

--- src/modules/tickets/domain/repositories/ITicketsRepository.ts
+++ src/modules/tickets/domain/repositories/ITicketsRepository.ts
@@ -1,9 +1,14 @@
 import { ITicket } from "../models/ITicket";
 import { ICreateTicket } from "../models/ICreateTicket";
+import { IFindByChairAndSession } from "../models/IFindByChairAndSession";
+import { IFindByIdAndSession } from "../models/IFindByIdAndSession";
+import { IUpdateSession } from "@modules/sessions/domain/models/IUpdateSession";
 
 export interface ITicketsRepository {
   findById(id: string): Promise<ITicket | null>;
+  findByIdAndSession({id, session_id}: IFindByIdAndSession): Promise<ITicket | null>
+  findByChairAndSession({chair, session_id}: IFindByChairAndSession): Promise<ITicket | null>
   createTicket({chair, value, session_id}: ICreateTicket): Promise<ITicket>;
-  updateTicket(ticket: ITicket): Promise<ITicket | null>;
+  updateTicket(ticket: IUpdateSession): Promise<ITicket | null>;
   deleteTicket(ticket: ITicket): Promise<void>; 
 }
