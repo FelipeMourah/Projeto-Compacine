7b4e1cb29db0fe60e5f30284b4e6b7b05c3a3136
Valmir Thume Junior <juninhothume21@gmail.com>
2024-06-08 11:24:57 -0300

    feat: Add List(all) and Show(only one) tickets features

---
+++ src/modules/tickets/services/ListTicketService.ts
@@ -0,0 +1,20 @@
+import { inject, injectable } from "tsyringe";
+import { DataSource } from "typeorm";
+import { ITicketsRepository } from "../domain/repositories/ITicketsRepository";
+import { ITicket } from "../domain/models/ITicket";
+
+
+@injectable()
+class ListTicketService {
+  constructor(
+    @inject('TicketRepository')
+    private ticketRepository: ITicketsRepository
+  ){}
+
+  public async execute(): Promise<ITicket[] | null> {
+    const tickets = await this.ticketRepository.findAll();
+    return tickets;
+  }
+}
+
+export default ListTicketService;
