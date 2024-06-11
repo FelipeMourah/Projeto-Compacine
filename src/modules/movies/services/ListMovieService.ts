a0e38cf24b0fbf444f2ee3c0b62292850658fb64
EmanuelErnesto <emanuel.earego@gmail.com>
2024-06-11 17:11:32 -0300

    Revert "fix: date formatation condition"
    This reverts commit 2e00819bb6596531363a68f9a681986525817b57.

--- src/modules/movies/services/ListMovieService.ts
+++ src/modules/movies/services/ListMovieService.ts
@@ -14,26 +14,25 @@ class ListMovieService {
   public async execute(): Promise<Movie[]> {
     const movies = await this.moviesRepository.findAll();
 
-    return movies.map(movie => {
+    const formattedMovies: Movie[] = movies.map(movie => {
       const newDate = addDays(movie.release_date, 1);
       const formattedDay = format(newDate, 'dd/MM/yyyy');
-      const formattedSessionDay =
-        movie.sessions && Array.isArray(movie.sessions)
-          ? movie.sessions.map(session => {
-              const newSessionDay = addDays(session.day, 1);
+      const formattedSessionDay = movie.sessions.map(session => {
+        const newSessionDay = addDays(session.day, 1);
 
-              return {
-                ...session,
-                day: format(newSessionDay, 'dd/MM/yyyy'),
-              };
-            })
-          : [];
+        return {
+          ...session,
+          day: format(newSessionDay, 'dd/MM/yyyy'),
+        };
+      });
       return {
         ...movie,
         release_date: formattedDay,
         sessions: formattedSessionDay,
       };
     });
+
+    return formattedMovies;
   }
 }
 
